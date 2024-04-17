db703de71dad7dcb4f528b2e8a8071b33c551ef3
cuteolaf <oliverlim818@gmail.com>
2024-04-17 22:31:11 +0000

    cargo fmt again

--- runtime/regionx/src/ismp.rs
+++ runtime/regionx/src/ismp.rs
@@ -85,9 +85,8 @@ impl IsmpModule for ProxyModule {
 
 		#[allow(clippy::match_single_binding)]
 		match pallet_id {
-			pallet_regions::PALLET_ID => {
-				pallet_regions::IsmpModuleCallback::<Runtime>::default().on_response(response)
-			},
+			pallet_regions::PALLET_ID => 
+				pallet_regions::IsmpModuleCallback::<Runtime>::default().on_response(response),
 			_ => Err(Error::ImplementationSpecific("Destination module not found".to_string())),
 		}
 	}
@@ -104,9 +103,8 @@ impl IsmpModule for ProxyModule {
 
 		#[allow(clippy::match_single_binding)]
 		match pallet_id {
-			pallet_regions::PALLET_ID => {
-				pallet_regions::IsmpModuleCallback::<Runtime>::default().on_timeout(timeout)
-			},
+			pallet_regions::PALLET_ID => 
+				pallet_regions::IsmpModuleCallback::<Runtime>::default().on_timeout(timeout),
 			// instead of returning an error, do nothing. The timeout is for a connected chain.
 			_ => Ok(()),
 		}
