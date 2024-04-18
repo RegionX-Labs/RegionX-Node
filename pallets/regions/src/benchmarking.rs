3ad2fcf85e1738198840cb4ed7a1c08804e1eb29
Sergej <sakacszergej@gmail.com>
2024-04-18 16:31:38 +0200

    fix benchmarks

--- pallets/regions/src/benchmarking.rs
+++ pallets/regions/src/benchmarking.rs
@@ -20,36 +20,48 @@
 use super::*;
 
 use frame_benchmarking::v2::*;
-use crate::mock::*;
-use pallet_broker::{RegionId, CoreMask};
+use frame_support::{assert_ok, traits::nonfungible::Mutate};
+use frame_system::RawOrigin;
+use pallet_broker::{CoreMask, RegionId};
+
+const SEED: u32 = 0;
 
 fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
 
 #[benchmarks]
-mod benchmarks  {
+mod benchmarks {
 	use super::*;
 
 	#[benchmark]
 	fn transfer() -> Result<(), BenchmarkError> {
-		let old_owner = 1;
-		let new_owner: T::AccountId = 2;
+		let caller = whitelisted_caller();
+		let new_owner: T::AccountId = account("alice", 0, SEED);
 		let region_id = RegionId { begin: 112830, core: 72, mask: CoreMask::complete() };
 
+		// We first mint a region.
+		assert_ok!(crate::Pallet::<T>::mint_into(&region_id.into(), &caller));
+
 		#[extrinsic_call]
-		_(RuntimeOrigin::signed(old_owner), region_id, new_owner);
+		_(RawOrigin::Signed(caller), region_id, new_owner);
 
 		Ok(())
 	}
 
 	#[benchmark]
 	fn request_region_record() -> Result<(), BenchmarkError> {
-		let owner = 1;
+		let caller: T::AccountId = whitelisted_caller();
 		let region_id = RegionId { begin: 112830, core: 72, mask: CoreMask::complete() };
 
+		// Create a region with an unavailable record, allowing us to re-request the record.
+		crate::Regions::<T>::insert(
+			region_id,
+			Region { owner: caller.clone(), record: Record::<T>::Unavailable },
+		);
+
 		#[extrinsic_call]
-		_(RuntimeOrigin::signed(owner), region_id);
+		_(RawOrigin::Signed(caller), region_id);
 
 		Ok(())
 	}
