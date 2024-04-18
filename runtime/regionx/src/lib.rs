415f9c31b03886d9a7fb5c850738272a2bb56652
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2024-04-18 18:19:32 +0200

    Update runtime/regionx/src/lib.rs
    Co-authored-by: cuteolaf <OliverLim818@gmail.com>

--- runtime/regionx/src/lib.rs
+++ runtime/regionx/src/lib.rs
@@ -557,7 +557,7 @@ impl pallet_utility::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
 	type PalletsOrigin = OriginCaller;
-	type WeightInfo = ();
+	type WeightInfo = pallet_utility::weights::SubstrateWeight<Runtime>;
 }
 
 parameter_types! {
