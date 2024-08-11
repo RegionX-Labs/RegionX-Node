97d4d12de6a448987acaac40f26ce163117dfe21
Sergej <sakacszergej@gmail.com>
2024-08-11 09:23:27 +0200

    remove unused import

--- pallets/regions/src/lib.rs
+++ pallets/regions/src/lib.rs
@@ -418,7 +418,6 @@ impl<T: Config> Default for IsmpModuleCallback<T> {
 	}
 }
 
-use frame_support::ensure;
 impl<T: Config> IsmpModule for IsmpModuleCallback<T> {
 	fn on_accept(&self, _request: Post) -> Result<(), IsmpError> {
 		Err(IsmpCustomError::NotSupported.into())
