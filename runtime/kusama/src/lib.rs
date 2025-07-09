354bacc41a855f7c767c5614fc57d1c4c8133d8c
Sergej <sakacszergej@gmail.com>
2025-07-09 14:13:02 +0200

    resolve more todos

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -60,6 +60,8 @@ use sp_runtime::{
 };
 use staging_parachain_info as parachain_info;
 use staging_xcm as xcm;
+use staging_xcm_builder as xcm_builder;
+use staging_xcm_executor as xcm_executor;
 
 use sp_std::prelude::*;
 #[cfg(feature = "std")]
@@ -212,7 +214,6 @@ pub const MILLI_KSM: Balance = 1_000_000_000;
 pub const MICRO_KSM: Balance = 1_000_000;
 
 pub const fn deposit(items: u32, bytes: u32) -> Balance {
-	// TODO: ensure this is a sensible value.
 	items as Balance * MILLI_KSM + (bytes as Balance) * 10 * MICRO_KSM
 }
 
@@ -720,7 +721,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment = 11,
 
 		// Governance
-		Sudo: pallet_sudo = 20, // TODO: leave this only for testnets
+		Sudo: pallet_sudo = 20, // TODO: remove this later
 
 		// Collator support. The order of these 4 are important and shall not change.
 		Authorship: pallet_authorship = 40,
@@ -759,7 +760,6 @@ construct_runtime!(
 #[macro_use]
 extern crate frame_benchmarking;
 
-// TODO: Add missing modules to benchmarks.
 #[cfg(feature = "runtime-benchmarks")]
 mod benches {
 	define_benchmarks!(
