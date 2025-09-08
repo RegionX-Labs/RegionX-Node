a77de75feeb93c15f65832f80badf67118c4952b
Sergej <sakacszergej@gmail.com>
2025-09-08 17:44:00 +0200

    bump spec to 1_001_000

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -186,7 +186,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("regionx-kusama-parachain"),
 	impl_name: Cow::Borrowed("regionx-kusama-parachain"),
 	authoring_version: 1,
-	spec_version: 1_000_000,
+	spec_version: 1_001_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
